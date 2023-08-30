97b1ee4309ce8d9b1f0b5dcec637bb60508d4a70
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-08-30 08:24:25 -0300

    Creating the user domain

---
+++ webapi.locadora/Domains/UsuarioDomain.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.locadora.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade Usuario
+    /// </summary>
+    public class UsuarioDomain
+    {
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir e-mail!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir senha!")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O usuário deve possuir uma permição, seja comum ou seja administrador!")]
+        public bool IsAdmin { get; set; }
+    }
+}
