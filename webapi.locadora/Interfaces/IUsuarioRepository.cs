8d4221e65a571cc94396829733f439b48d360fe7
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-08-30 08:56:09 -0300

    Creating the user repository

---
+++ webapi.locadora/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using webapi.locadora.Domains;
+
+namespace webapi.locadora.Interfaces
+{
+    /// <summary>
+    /// Interface que define os métodos a serem implementados em o UsuarioRepository
+    /// </summary>
+    public interface IUsuarioRepository
+    {
+        /// <summary>
+        /// É o método que permite a autenticação do usuário
+        /// </summary>
+        /// <param name="email">O e-mail do usuário</param>
+        /// <param name="senha">A senha do usuário</param>
+        /// <returns>Os dados do usuário em um objeto</returns>
+        UsuarioDomain login(string email, string senha);
+    }
+}
---
+++ webapi.locadora/Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using webapi.locadora.Domains;
+using webapi.locadora.Interfaces;
+
+namespace webapi.locadora.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private string StringConexao = "Data Source = NOTE17-S14; Initial Catalog = Locadora; User Id = sa; Pwd = Senai@134";
+
+        /// <summary>
+        /// É o método que permite a autenticação do usuário
+        /// </summary>
+        /// <param name="email">O e-mail do usuário</param>
+        /// <param name="senha">A senha do usuário</param>
+        /// <returns>Os dados do usuário em um objeto</returns>
+        public UsuarioDomain login(string email, string senha)
+        {
+            using (SqlConnection connection = new SqlConnection(StringConexao))
+            {
+                string query = "SELECT IdUsuario, Nome, Email, Senha, IsAdmin FROM Usuario " +
+                                "WHERE Email = @Email AND Senha = @Senha";
+
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Senha", senha);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        UsuarioDomain usuarioEncontrado = new UsuarioDomain()
+                        {
+                            IdUsuario = Convert.ToInt32(reader["IdUsuario"]),
+                            Nome = reader["Nome"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            IsAdmin = Convert.ToBoolean(reader["IsAdmin"])
+                        };
+
+                        return usuarioEncontrado;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
