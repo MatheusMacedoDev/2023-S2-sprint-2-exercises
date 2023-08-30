17db6e5c7b879f5bda820ff7c1338a58b5b82d48
Matheus Macedo <matheus.macedo.developer@gmail.com>
2023-08-30 10:15:29 -0300

    Creating the user controller

---
+++ webapi.locadora/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.locadora.DataTransfering;
+using webapi.locadora.Domains;
+using webapi.locadora.Interfaces;
+using webapi.locadora.Repositories;
+
+namespace webapi.locadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UsuarioTransfer dadosUsuario)
+        {
+            try
+            {
+                UsuarioDomain usuarioEncontrado = _usuarioRepository.Login(dadosUsuario.Email, dadosUsuario.Senha);
+
+                if(usuarioEncontrado == null) { 
+                    return NotFound("Os dados não correspondem a nenhum usuário.");
+                }
+
+                return Ok(usuarioEncontrado);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err);
+            }
+        } 
+    }
+}
---
+++ webapi.locadora/DataTransfer/UsuarioTransfer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.locadora.DataTransfering
+{
+    public class UsuarioTransfer
+    {
+        [Required(ErrorMessage = "O e-mail não foi inserido!")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "A senha não foi inserida!")]
+        public string Senha { get; set; }
+    }
+}
